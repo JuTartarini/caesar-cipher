2c9cc57f46c6acc9dbecaec31c170dbe4072d3dc
ju.tartarini <ju.tartarini@gmail.com>
2019-01-28 10:47:27 -0200

    Alterações no codigo

--- src/cipher.js
+++ src/cipher.js
@@ -1,60 +1,49 @@
-function BtnCodificar()
-{
+function BtnCodificar() {
 
-let msgASerCodificada = document.getElementById('txtMessageCodificar').value.trim();
-document.getElementById('txtMessageCodificar').innerHTML = msgASerCodificada;
-let offset = document.getElementById('desloCodificar').value;
-document.getElementById('numMessageCodifica').innerHTML = offset;
+  let msgASerCodificada = document.getElementById('txtMessageCodificar').value.trim();
+  document.getElementById('txtMessageCodificar').innerHTML = msgASerCodificada;
+  let offset = document.getElementById('desloCodificar').value;
 
+  let msgCodificada;
+  let res = "";
 
-//let caracter = msgASerCodificada.substring(3,4);
-//let msgConvertidaArray = Array.from(msgASerCodificada);
-//console.log(msgConvertidaArray);
-//console.log(msgConvertidaArray[0]);
+  for (let i = 0; i < msgASerCodificada.length; i++)
+  {
+    let posicaoLetraASCII = msgASerCodificada.charCodeAt(i);
+    //if (posicaoLetraASCII >= 65 && posicaoLetraASCII <= 90)
+    //{
+      msgCodificada = ((posicaoLetraASCII - 65 + parseInt(offset)) % 26) + 65
+      res += String.fromCharCode(msgCodificada);
+        console.log(posicaoLetraASCII);
+    //}
+  }
 
-
-
-
-let tamanhoAlfabet = 90 - 65;
-let msgCodificada;
-
-let res = "";
-for (let i = 0; i < msgASerCodificada.length; i++)
-{
-let posicaoLetraASCII = msgASerCodificada.charCodeAt(i);
-//if (posicaoLetraASCII >= 65 && posicaoLetraASCII <= 90)
-//{
-
- msgCodificada = ((posicaoLetraASCII - 65 + parseInt(offset)) % 26) + 65
- res += String.fromCharCode(msgCodificada);
-//}
-
-
+  document.getElementById("lblMessageCodifica").innerHTML = res;
 }
-
-document.getElementById("lblMessageCodifica").innerHTML = res;
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
 
 function BtnDesCodificar()
 {
+  let msgASerDesCodificada = document.getElementById('txtMessageDesCodificar').value.trim();
+  document.getElementById('txtMessageDesCodificar').innerHTML = msgASerDesCodificada;
+  let offset = document.getElementById('desloDesCodificar').value;
 
-  let msgDescodificada = document.getElementById('txtMessageDesCodificar').value.trim();
-  document.getElementById('txtMessageDesCodificar').innerHTML = msgDescodificada;
-  let res = msgDescodificada.charCodeAt(msgDescodificada[1]);
-  document.getElementById("lblMessageDesCodifica").innerHTML = res;
+  let msgDesCodificada;
+  let res2 = " ";
 
+  for (let i = 0; i < msgASerDesCodificada.length; i++)
+  {
+    let posicaoLetraASCII = msgASerDesCodificada.charCodeAt(i);
+
+      msgDesCodificada = ((posicaoLetraASCII - 65 - parseInt(offset)) % 26) + 65
+      res2 += String.fromCharCode(msgDesCodificada);
+      console.log(posicaoLetraASCII);
+      
+      console.log(res2);
+
+  }
+
+
+
+  document.getElementById("lblMessageDesCodifica").innerHTML = res2;
 
 }
